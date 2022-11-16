1161424e94ea94bfb2b6855182da2e79764d93db
Jurij Jusel <jjusel@gmail.com>
2022-11-16 22:40:13 +0200

    pridetas jurij_scrape.py

---
+++ jurij_scrape.py
@@ -0,0 +1,78 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+
+# user_input = input("Iveskite paieskos zodi fraze: ")
+# template = "https://www.cvbankas.lt/?location=&padalinys%5B%5D=&keyw={}"
+
+# def get_url(keyword):
+#     template = "https://www.cvbankas.lt/?location=&padalinys%5B%5D=&keyw={}"
+#     url = template.format(keyword)
+#     return url
+                       
+# url = get_url(user_input)                         
+   
+   
+# source = requests.get(url).text
+# soup = BeautifulSoup(source, 'lxml')
+# articles = soup.find_all('article')
+                          
+source = requests.get('https://www.cvbankas.lt/?location=606&padalinys%5B%5D=76&keyw=python').text
+
+soup = BeautifulSoup(source, 'lxml')
+
+articles = soup.find_all('article')
+print(len(articles)) 
+print(type(articles))
+print(articles)
+
+# spans = soup.find_all('span', {'class': 'dib mt5'})
+# for span in spans:
+#     print(span.text)
+
+
+article = articles[13]
+position = article.h3.text
+company = article.find('span', {'class': 'dib mt5'}).text
+date = article.find('span', {'class': 'txt_list_2'}).text
+salary = article.find('span', {'class': 'salary_amount'}).text
+city = article.find('span', {'class': 'list_city'}).text
+
+article = articles[14]
+position1 = article.h3.text
+company1 = article.find('span', {'class': 'dib mt5'}).text
+date1 = article.find('span', {'class': 'txt_list_2'}).text
+salary1 = article.find('span', {'class': 'salary_amount'}).text
+city1 = article.find('span', {'class': 'list_city'}).text
+
+# out =[]
+out_posit_13 = f"miestas: {city}, kompanija: {company}, pozicija: {position}, atlyginimas: {salary}, paskelbimo data: {date}"
+print(out_posit_13)
+# print("- " * 20)
+out_posit_14 = f'miestas: {city1}, kompanija: {company1}, pozicija: {position1}, atlyginimas: {salary1}, paskelbimo data: {date1}'
+print(out_posit_14)
+# print(dict(out_posit_14))
+# out.append(out_posit_14)
+# out.append(out_posit_13)
+# str_out = str(out)
+# print(str(str_out))
+
+# with open("out_data.json", "w") as outfile:
+#     json.dump(out_posit_13,outfile)
+#     json.dump(out_posit_14, outfile)
+    
+# with open("out_data.json", "w") as outfile:
+#     json.dump(str_out, outfile, indent=3)
+#     json.dump(out_posit_14, outfile)
+
+# with open("out_data.json", "w") as outfile:
+#     outfile.write(str_out)
+#     json.dump(out_posit_14, outfile)
+
+
+
+# with open('web_scrape.txt', 'w') as file:
+#     file.write(out_posit_13)
+#     file.write(out_posit_14)
+#     print(f'Informacija irasyta faile ')
+
