68f1738f19359f0a06fe29b475a09f026368e996
Jurij Jusel <jjusel@gmail.com>
2022-11-27 10:19:09 +0200

    add def upload_time

--- jurij_scrape.py
+++ jurij_scrape.py
@@ -2,7 +2,6 @@ from bs4 import BeautifulSoup
 import requests
 import json
 import time
-from datetime import datetime, timedelta
 
 
 def count_time():
@@ -10,7 +9,7 @@ def count_time():
         return round(duration, 2)
 
 
-start_time = time.time() 
+start_time = time.perf_counter() 
                          
 source = requests.get('https://www.cvbankas.lt/?location=606&padalinys%5B%5D=76&keyw=python').text
 soup = BeautifulSoup(source, 'lxml')
@@ -19,7 +18,7 @@ articles = soup.find_all('article')
 print(f"amount of post:", len(articles)) 
 
 
-article = articles[20]
+article = articles[25]
 post_id = article.find('div', {'class': 'jobadlist_ad_anchor'}).get("id")[6:]
 post_url = article.find("a", {"class": "list_a can_visited list_a_has_logo"}).attrs['href'] 
 img_url = article.find('img').get('src')
@@ -30,24 +29,34 @@ salary = article.find('span', {'class': 'salary_amount'}).text
 city = article.find('span', {'class': 'list_city'}).text
 
 
+current_time = round(time.time())
 post_date_split = post_date.split()
 data_number = post_date_split[1]
+current_time = round(time.time())
+time_values = ['val.', 'min.', 'd.']
 
-if 'd.' in post_date_split:
-        post_temp = datetime.now() - timedelta(days=int(data_number))
-        post_upload = post_temp.ctime()
-       
-elif "val." in post_date_split:
-        post_temp = datetime.now() - timedelta(hours=int(data_number))
-        post_upload = post_temp.ctime()
-        
-elif "min." in post_date_split:
-        post_temp = datetime.now() - timedelta(minutes=int(data_number))
-        post_upload = post_temp.ctime()
-        
-else:
-        post_upload = post_date
+def upload_time():
+        for x in post_date_split:
+                if x in time_values:
+                        if x == "d.":
+                                temp_days = int(data_number) * 86400
+                                post_upload = current_time - temp_days
+                                return post_upload
+                        
+                        if x == "val.":
+                                temp_hour = int(data_number) * 3600
+                                post_upload = current_time - temp_hour
+                                return post_upload
+                        
+                        if x == "min.":
+                                temp_min = int(data_number) * 60
+                                post_upload = current_time - temp_min
+                                return post_upload
+                        else:
+                                return  # neveikia !!!!! palikau 
 
+                                  
+up = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(upload_time()))
 
 
 post = {
@@ -58,12 +67,12 @@ post = {
         "company": company, 
         "salary": salary,
         "city": city,
-        "time_passed": post_date,
-        "upload_time": post_upload
+         "upload_post": up,
+        "time_public": post_date
         }
 print(post)
 
-stop_time = time.time()  
+stop_time = time.perf_counter()   
 
 
 with open("cv_bankas_scrap.json", "a") as file:
