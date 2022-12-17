185521c833ea37cdc1d3c6de16518aa1fc9e89a6
Jurij Jusel <jjusel@gmail.com>
2022-12-17 20:49:32 +0200

    change post_description_full, try func relocate

--- jurij_scrape.py
+++ jurij_scrape.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import json
 import time
-from srap_preprocessing import upload_time, count_time
+from srap_preprocessing import upload_time, count_time, try_salary, try_applicants, try_post_date, create_json
 
 
 start_time = time.perf_counter() 
@@ -13,6 +13,7 @@ soup = BeautifulSoup(source, 'lxml')
 articles = soup.find_all('article')
 print(f"amount of post:", len(articles)) 
 
+
 posts_list = []
 for article in articles:
     post_id = article.find('div', {'class': 'jobadlist_ad_anchor'}).get("id")[6:]
@@ -21,52 +22,24 @@ for article in articles:
     position = article.find('h3').text
     company = article.find('span', {'class': 'dib mt5'}).text
     city = article.find('span', {'class': 'list_city'}).text
-
-    try:
-        salary = article.find('span', class_ = 'salary_amount').text
-        salary_split = salary.split('-')
-        salary_int =list(map(int, salary_split))
-    except Exception:
-        salary = "0"
-        
-        
-    try:
-        post_date = article.find('span', {'class': 'txt_list_2'}).text
-    except:
-        post_date = article.find('span', class_ = 'txt_list_important').text      
-            
-    
-                                    
+    salary = try_salary(article)
+    post_date = try_post_date(article)                           
     upload_post = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(upload_time(post_date)))
 
 
     post_source = requests.get(post_url)
     post_soup = BeautifulSoup(post_source.text, 'lxml')
-        
-    try:
-        applicants_full = post_soup.find_all('div', class_='jobad_stat')[1].text
-        applicants_value = applicants_full.split()[0]
-    except:
-        applicants_value = "0"
-
-    post_descript_full = post_soup.find_all('div', {'class': 'jobad_txt'})
-    post_descript = post_descript_full[0].text, post_descript_full[1].text
-
-    # post_descript_full[1].text
-    # post_descript_full[2].text
-    # post_descript_full[3].text
-    # post_descript_full[4].text 
-    # post_descript_full[5].text
-
+    post_description_full= post_soup.find_all('section',itemprop='description')[0].get_text()   
+    applicants_value = try_applicants(post_soup)
 
     post_data = {
-        "post_descrip": post_descript,
+        "post_descrip": post_description_full,
         "post_id": post_id,
         "applicants": applicants_value,
+        "company": company, 
+        "position": position,
         "post_url": post_url, 
         "img_url": img_url,
-        "position": position, 
-        "company": company, 
         "salary": salary,
         "city": city,
         "upload_post": upload_post,
@@ -75,20 +48,16 @@ for article in articles:
     
     posts_list.append(post_data)
     
-print(posts_list)
+# print(posts_list)
 print('Post scraping done.')
 
-# for i in range(len(posts_list)):
-#     print()
-#     print(i, posts_list[i])
+for i in range(len(posts_list)):
+    print()
+    print(i, posts_list[i])
 
 
 stop_time = time.perf_counter()   
 
-
-# with open("cv_bankas_scrap.json", "a") as file:
-#     json.dump(post, file, indent=2)
-#     print(f'Informacija irasyta cv_bankas_scrap.json faile')
-
+# print(create_json(posts_list))
 
 print(f'web information extraction time',count_time(start_time, stop_time))
