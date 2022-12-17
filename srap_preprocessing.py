2eb409dd137f314bd5d27eb3c3dca7d9d45c7eba
Jurij Jusel <jjusel@gmail.com>
2022-12-17 20:46:46 +0200

    add func try_post_date, try_applicants, try_salary

--- srap_preprocessing.py
+++ srap_preprocessing.py
@@ -1,7 +1,16 @@
 import time
 from pathlib import Path
 import json
-     
+
+
+def try_post_date(article):
+    try:
+        post_date = article.find('span', {'class': 'txt_list_2'}).text  
+    except:
+        post_date = article.find('span', class_ = 'txt_list_important').text      
+    return post_date   
+
+    
 def upload_time(post_date):
      
     current_time = round(time.time())
@@ -28,8 +37,27 @@ def upload_time(post_date):
                 return post_upload
     else:
         return current_time
-  
-  
+ 
+ 
+def try_applicants(post_soup):   
+    try:
+        applicants_full = post_soup.find_all('div', class_='jobad_stat')[1].text
+        applicants_value = applicants_full.split()[0]
+    except:
+        applicants_value = "0"
+    return applicants_value  
+
+
+def try_salary(article):
+    try:
+        salary = article.find('span', class_ = 'salary_amount').text
+        salary_split = salary.split('-')
+        salary =list(map(int, salary_split))
+    except Exception:
+        salary = [0]
+    return salary
+
+
 def create_json(posts_list):
     json_file = "data.json"
     path = Path(json_file)
