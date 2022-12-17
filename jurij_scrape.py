d1dba6ff5b012a6118168578ce83d46a2082c6bb
Jurij Jusel <jjusel@gmail.com>
2022-12-17 17:56:29 +0200

    for cicle try salsary, post date, applicants

--- jurij_scrape.py
+++ jurij_scrape.py
@@ -13,73 +13,74 @@ soup = BeautifulSoup(source, 'lxml')
 articles = soup.find_all('article')
 print(f"amount of post:", len(articles)) 
 
+posts_list = []
+for article in articles:
+    post_id = article.find('div', {'class': 'jobadlist_ad_anchor'}).get("id")[6:]
+    post_url = article.find("a", {"class": "list_a can_visited list_a_has_logo"}).attrs['href'] 
+    img_url = article.find('img').get('src')
+    position = article.find('h3').text
+    company = article.find('span', {'class': 'dib mt5'}).text
+    city = article.find('span', {'class': 'list_city'}).text
 
-article = articles[24]
-post_id = article.find('div', {'class': 'jobadlist_ad_anchor'}).get("id")[6:]
-post_url = article.find("a", {"class": "list_a can_visited list_a_has_logo"}).attrs['href'] 
-img_url = article.find('img').get('src')
-position = article.find('h3').text
-company = article.find('span', {'class': 'dib mt5'}).text
-post_date = article.find('span', {'class': 'txt_list_2'}).text
-city = article.find('span', {'class': 'list_city'}).text
-
-try:
-    salary = article.find('span', {'class': 'salary_amount'}).text
-except Exception as e:
-    salary = "0"
+    try:
+        salary = article.find('span', class_ = 'salary_amount').text
+        salary_split = salary.split('-')
+        salary_int =list(map(int, salary_split))
+    except Exception:
+        salary = " "
         
         
-salary_split = salary.rsplit('-')
-salary_int =list(map(int, salary_split))
-  
-                                  
-upload_post = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(upload_time(post_date)))
-
-post_source = requests.get(post_url)
-post_soup = BeautifulSoup(post_source.text, 'lxml')
-
-
-job_h2 = post_soup.find_all('h2')
-job_full = post_soup.find_all('div', {'class': 'jobad_txt'})
-
-job_head_desc = job_h2[0].text
-job_descrip = post_soup.find('div', {'class': 'jobad_txt'}).text
-job_head_candid = job_h2[1].text
-job_cand_full= job_full[1]
-job_candidate = job_cand_full.text
-job_head_requir = job_h2[2].text
-job_reguir_ful = job_full[2]
-job_requirement = job_reguir_ful.text
-job_head_salary = job_h2[3].text
-job_salary_full = job_full[3]
-job_sal= job_salary_full.text
-job_sal_format = (''.join([ch for ch in job_sal if ch not in [' ', '\t', '\n']]))
+    try:
+        post_date = article.find('span', {'class': 'txt_list_2'}).text
+    except:
+        post_date = article.find('span', class_ = 'txt_list_important').text      
+            
+    
+                                    
+    upload_post = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(upload_time(post_date)))
 
 
+    post_source = requests.get(post_url)
+    post_soup = BeautifulSoup(post_source.text, 'lxml')
+        
+    try:
+        applicants_full = post_soup.find_all('div', class_='jobad_stat')[1].text
+        applicants_value = applicants_full.split()[0]
+    except:
+        applicants_value = "0"
+
+    post_descript_full = post_soup.find_all('div', {'class': 'jobad_txt'})
+    post_descript = post_descript_full[0].text, post_descript_full[1].text
+
+    # post_descript_full[1].text
+    # post_descript_full[2].text
+    # post_descript_full[3].text
+    # post_descript_full[4].text 
+    # post_descript_full[5].text
 
 
-post = {
-    "job_head_desc": job_head_desc,
-    "job_descrip": job_descrip,
-    "job_head_candid":job_head_candid,
-    "job_candidate": job_candidate,
-    "job_head_requirement": job_head_requir,
-    "job_requirement": job_requirement,
-    "job_head_salary": job_head_salary,
-    "job_salary_format": job_sal_format,
-    "post_id": post_id,
-    "post_url": post_url, 
-    "img_url": img_url,
-    "position": position, 
-    "company": company, 
-    "salary": salary_int,
-    "city": city,
-    "upload_post": upload_post,
-    "time_public": post_date
-        }
-
-print(post)
+    post_data = {
+        "post_descrip": post_descript,
+        "post_id": post_id,
+        "applicants": applicants_value,
+        "post_url": post_url, 
+        "img_url": img_url,
+        "position": position, 
+        "company": company, 
+        "salary": salary,
+        "city": city,
+        "upload_post": upload_post,
+        "time_public": post_date
+            }
+    
+    posts_list.append(post_data)
+    
+print(posts_list)
 print('Post scraping done.')
+
+# for i in range(len(posts_list)):
+#     print()
+#     print(i, posts_list[i])
 
 
 stop_time = time.perf_counter()   
