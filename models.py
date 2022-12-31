e6b44cc2fa08602c458fb07fed549287e7dc5d42
Jurij Jusel <jjusel@gmail.com>
2022-12-31 21:37:41 +0200

    models.py addition

--- models.py
+++ models.py
@@ -20,7 +20,32 @@ class Post:
         self.upload_post = upload_post
         self.post_date = post_date
         self.applicants_value = applicants_value
-        self.date_time_now = time_now
+        self.time_now = time_now
         
         
-# Kol kas padariau tik inicializacija bandau aiskintis ir pasimokint classes
+    def data_dict(self):
+        data = {  
+            'post_id': post.post_id,
+            'post_url': post.post_url,
+            'position': post.position,
+            'company': post.company,
+            'city': post.city,
+            'salary': post.salary,
+            'upload_post': post.upload_post,           
+            'applicants_value': post.applicants_value,
+            'post_description_full': post.post_description_full,
+            'post_date': post.post_date,
+            'date_time_now': post.time_now,
+        }
+        return data
+
+
+    def __str__(self):
+        return f"post_description_full: {self.post_description_full}, post_id: {self.post_id}, post_url = {self.post_url}, position: {self.position}, company: {self.company}, city: {self.city}, salary: {self.salary}, upload_post: {self.upload_post}, post_date: {self.post_date}, applicants_value: {self.applicants_value}, time_now: {self.time_now}"
+     
+
+post = Post('1','2','3','4','5','6','7','8','9','10','11')
+post1 = Post('a','b','c','d','i','f','g','h','j','k','l')
+# print(x.city)
+print(post.data_dict())
+print(post1)        
