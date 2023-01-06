4e8dc901c61f91f6d371bbf8d0bf4d4f0e3cfe74
Jurij Jusel <jjusel@gmail.com>
2023-01-06 22:16:58 +0200

    post  change to self

--- models.py
+++ models.py
@@ -25,17 +25,17 @@ class Post:
         
     def data_dict(self):
         data = {  
-            'post_id': post.post_id,
-            'post_url': post.post_url,
-            'position': post.position,
-            'company': post.company,
-            'city': post.city,
-            'salary': post.salary,
-            'upload_post': post.upload_post,           
-            'applicants_value': post.applicants_value,
-            'post_description_full': post.post_description_full,
-            'post_date': post.post_date,
-            'date_time_now': post.time_now,
+            'post_id': self.post_id,
+            'post_url': self.post_url,
+            'position': self.position,
+            'company': self.company,
+            'city': self.city,
+            'salary': self.salary,
+            'upload_post': self.upload_post,           
+            'applicants_value': self.applicants_value,
+            'post_description_full': self.post_description_full,
+            'post_date': self.post_date,
+            'date_time_now': self.time_now,
         }
         return data
 
@@ -47,5 +47,6 @@ class Post:
 post = Post('1','2','3','4','5','6','7','8','9','10','11')
 post1 = Post('a','b','c','d','i','f','g','h','j','k','l')
 # print(x.city)
-print(post.data_dict())
-print(post1)        
+# print(post.data_dict())
+# print(post1.data_dict())
+       
