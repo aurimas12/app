98a81ae6abbe3bbdf47a78c0016ff0370af64d1b
Jurij Jusel <jjusel@gmail.com>
2023-01-22 00:43:48 +0200

    changes app.py models.py,  srap_preprocessing.py

--- app.py
+++ app.py
@@ -1,36 +1,32 @@
 from bs4 import BeautifulSoup
 import requests
 from tqdm import tqdm
-from srap_preprocessing import upload_time, count_time, try_salary, try_applicants, try_post_date, count_pages, count_posts
+from srap_preprocessing import upload_time, count_time, try_salary, try_applicants, try_post_date, stopwatch_time, count_posts
 import time
+from models import Post
+import pandas as pd
+from utils.file import create_csv, read_csv, create_json
 
 class Crawler:
     def __init__(self, url):
         self.url = url
-        # self.header = HEADERS
-        self.pages = []
-        self. data = []
+        self.csv_file = 'data.csv'
+        self.json_file = 'data.json'
         self.posts = []
+        self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'}
 
 
     def download_url(self):
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'}
-        # response = requests.get(url.strip(), headers=headers, timeout=1)
-        response = requests.get(self.url, headers=headers, timeout=1)
-        # source = requests.get('https://www.cvbankas.lt/?page={page}')
-        # return response
-        print(response)
+        response = requests.get(self.url, headers=self.headers)  # timeout=1
+        return response.headers
+        
 
-
-    def download_content(self, pages_index): 
-        # last_page_index = x.get_last_page_index()
-        print(f'page index {pages_index}')
-        for page in tqdm(range(1, 2), ncols=100, colour="green", desc='Pages scraping progress'):
-            full_source = requests.get(f'{self.url}{page}')
-            full_soup = BeautifulSoup(full_source.text, 'lxml')
+    def download_content(self, pages_index):
+        print('total pages:', pages_index) 
+        for page in tqdm(range(1, pages_index+1), ncols=100, colour="green", desc='Pages scraping progress'):
+            full_source = requests.get(f'{self.url}{page}', headers=self.headers)
+            full_soup = BeautifulSoup(full_source.content, 'lxml')
             articles = full_soup.find_all('article')
-             
             for article in tqdm(articles, ncols=100, colour="yellow", desc='Posts scraping progress', leave=False):
                 post_id = article.find('div', {'class': 'jobadlist_ad_anchor'}).get("id")[6:]
                 post_url = article.find("a", {"class": "list_a can_visited list_a_has_logo"}).attrs['href']
@@ -41,27 +37,15 @@ class Crawler:
                 salary = try_salary(article)
                 post_date = try_post_date(article)
                 upload_post = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(upload_time(post_date)))
-
+                
                 post_source = requests.get(post_url)
-                post_soup = BeautifulSoup(post_source.text, 'lxml')
-                post_description_full = post_soup.find_all('section', itemprop='description')[0].get_text()
+                post_soup = BeautifulSoup(post_source.content, 'lxml')
+                description_full = post_soup.find_all('section', itemprop='description')[0].get_text()
                 applicants_value = try_applicants(post_soup)
 
-                post_data = {
-                    "post_descrip": post_description_full,
-                    "post_id": post_id,
-                    "applicants": applicants_value,
-                    "company": company,
-                    "position": position,
-                    "post_url": post_url,
-                    "img_url": img_url,
-                    "salary": salary,
-                    "city": city,
-                    "upload_post": upload_post,
-                    "time_public": post_date
-                }
-                
-                self.posts.append(post_data)           
+                post = Post(post_id,post_url,img_url,position,company,city,salary,post_date,upload_post,description_full,applicants_value)
+               
+                self.posts.append(post.data_dict())   
         return self.posts
     
 
@@ -73,8 +57,20 @@ class Crawler:
                 soup = BeautifulSoup(req.content, 'lxml')
                 if soup.select_one('[rel=next]') is None:
                     break
-                page_index += 1               
-        return page_index + 1
+                page_index += 1         
+        return page_index
+
+    
+    def create_df(self):
+        data_csv = {
+            'website': "www.cvbankas.lt",
+            'extract_time': self.count_time,
+            'total_posts': self.count_posts,
+            'posts': [self.posts],
+            'created_date': self.time_now
+        }
+        df = pd.DataFrame(data_csv, columns=['website', 'extract_time', 'total_posts', 'posts', 'created_date'])
+        return df
 
 
     def crawl(self):
@@ -82,11 +78,22 @@ class Crawler:
         page_content = self.download_content(pages_index)
         return page_content
     
-    
+      
     def run(self):
+        start_time = time.perf_counter()
+        self.time_now = time.strftime("%Y-%m-%d %H:%M:%S")
+        print(self.time_now)
+        self.count_posts = count_posts()
+        print('total posts:', self.count_posts)
         self.crawl()
-        
+        stop_time = time.perf_counter()
+        self.count_time = count_time(start_time, stop_time)
+        # print(create_json(self.posts, self.json_file))
+        print(create_csv(self.csv_file, self.create_df()))
+        print(read_csv(self.csv_file))
+        print(len(self.posts))
+        print('web information extraction time', stopwatch_time(start_time, stop_time))
+
 
 if __name__ == '__main__':
-    Crawler(url = 'https://www.cvbankas.lt?page=').run()
-    pass
+    Crawler(url='https://www.cvbankas.lt?page=').run()
--- models.py
+++ models.py
@@ -2,48 +2,40 @@ class Post:
     def __init__(self,
                  id, 
                  url, 
+                 imag_url,
                  position, 
                  company, 
-                 city, salary, 
+                 city, 
+                 salary, 
+                 post_date, 
                  upload_post, 
-                 create_date, 
                  description_full, 
-                 applicants_value, 
-                 time_now):
-        self.description_full = description_full
+                 applicants_value):
         self.id = id
         self.url = url
+        self.imag_url = imag_url
         self.position = position
         self.company = company
         self.city = city
         self.salary = salary
         self.upload_post = upload_post
-        self.date = create_date
+        self.date = post_date
+        self.description_full = description_full
         self.applicants_value = applicants_value
-        self.time_now = time_now
         
         
     def data_dict(self):
         data = {  
             'post_id': self.id,
             'post_url': self.url,
+            'imag_url': self.imag_url,
             'position': self.position,
             'company': self.company,
             'city': self.city,
             'salary': self.salary,
+            'post_date': self.date,
             'upload_post': self.upload_post,           
             'applicants_value': self.applicants_value,
             'post_description_full': self.description_full,
-            'post_date': self.create_date,
-            'date_time_now': self.time_now,
         }
         return data
-
-
-    def __str__(self):
-        return f"post_description_full: {self.description_full}, post_id: {self.id}, post_url = {self.url}, position: {self.position}, company: {self.company}, city: {self.city}, salary: {self.salary}, upload_post: {self.upload_post}, post_date: {self.post_date}, applicants_value: {self.applicants_value}, time_now: {self.time_now}"
-     
-
-post = Post('1','2','3','4','5','6','7','8','9','10','11')
-post1 = Post('a','b','c','d','i','f','g','h','j','k','l')
-
