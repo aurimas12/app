119f85f1874cc75258c7d60e8d86a909eaa2d25d
Jurij Jusel <jjusel@gmail.com>
2023-02-18 01:35:18 +0200

    changes

--- utils/unit_test.py
+++ utils/unit_test.py
@@ -1,2 +1,11 @@
-import unit_test
+import unittest
 from app import Crawler
+
+class TestCrawler(unittest.TestCase):
+    
+    def test_download_url(self):
+        down=Crawler(url='https://www.cvbankas.lt?page=')
+        self.assertEqual(down.download_url())
+
+if __name__ == '__main__':
+    unittest.main()
