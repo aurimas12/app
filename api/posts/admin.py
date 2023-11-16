2b30bd6fcde3c42819db59322433909f4f177cb0
Jurij Jusel <jjusel@gmail.com>
2023-11-16 21:34:15 +0200

    change tags, description, posts class

--- api/posts/admin.py
+++ api/posts/admin.py
@@ -1,27 +1,22 @@
 from django.contrib import admin
-from .models import Post, Tags, Description, Position
+from .models import Post, Tags, Description
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('register_id', 'post_url', 'upload_post', 'salary', 'created_datetime', 'company',)
-    search_fields = ('register_id', 'salary', 'upload_post', 'created_datetime',) 
+    list_display = ('register_id', 'post_url', 'position', 'publish_date', 'salary_min', 'salary_max', 'company',)
+    search_fields = ('register_id', 'salary_min', 'salary_max', 'position', 'publish_date', 'created_datetime',) 
+    readonly_fields = ('created_datetime',)
 
 
 @admin.register(Tags)
 class TagsAdmin(admin.ModelAdmin):
-    list_display = ['language', 'post']
+    list_display = ['language']
     search_fields = ['language']
 
 
 @admin.register(Description)
 class DescriptionAdmin(admin.ModelAdmin):
-    list_display = ['text', 'post']
+    list_display = ['text']
     search_fields = ['text']
 
-
-@admin.register(Position)
-class PositionAdmin(admin.ModelAdmin):
-    list_display = ['position', 'applicants_value']
-    search_fields = ['position', 'applicants_value']
- 
--- api/posts/models.py
+++ api/posts/models.py
@@ -2,35 +2,32 @@ from django.db import models
 from api.company.models import Company    
 
 
-class Post(models.Model):
-    register_id = models.CharField(max_length=40)  # post_id keiciamas i register_id
-    post_url = models.CharField(max_length=200)
-    upload_post = models.CharField(max_length=50)
-    salary = models.SmallIntegerField()
-    created_datetime = models.DateTimeField(auto_now_add=True,)
-    # Add foreign key to Company model with a default value
-    company = models.ForeignKey(Company, on_delete=models.CASCADE, default=1)
-    
-
 class Tags(models.Model):
     language = models.CharField(max_length=40)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, default=1)
 
 
 class Description(models.Model):
     text = models.TextField()
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, default=1)
-
-
-class Position(models.Model):
-    position = models.CharField(max_length=10)
-    applicants_value = models.SmallIntegerField()
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, default=1)
-    
-    
-    
     
 
+class Post(models.Model):
+    register_id = models.IntegerField(null=True)  # post_id keiciamas i register_id
+    post_url = models.CharField(max_length=200)
+    position = models.CharField(max_length=200)
+    publish_date = models.DateTimeField(auto_now_add=True)
+    salary_min = models.IntegerField(null=True)
+    salary_max = models.IntegerField(null=True)
+    created_datetime = models.DateTimeField(auto_now_add=True)
+    # Add foreign key to Company, Tags, Description model with default value
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, default=1)
+    tags = models.ForeignKey(Tags, on_delete=models.CASCADE, default=1)
+    description = models.ForeignKey(Description, on_delete=models.CASCADE, default=1)
+
+
+
+
+
+    
 # class Postsfull(models.Model):
 #     company_info = models.ForeignKey(CompanyInfo, on_delete=models.CASCADE)  # 'app_label.CompanyInfo'
 #     post = models.ForeignKey(Post, on_delete=models.CASCADE)  # 'app_label.Post'
