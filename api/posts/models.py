9ac571b06bd4df1bb4a9e43c9125b69aba0f3d17
Jurij Jusel <jjusel@gmail.com>
2023-11-16 21:59:16 +0200

    del comments

--- api/posts/models.py
+++ api/posts/models.py
@@ -18,26 +18,6 @@ class Post(models.Model):
     salary_min = models.IntegerField(null=True)
     salary_max = models.IntegerField(null=True)
     created_datetime = models.DateTimeField(auto_now_add=True)
-    # Add foreign key to Company, Tags, Description model with default value
-    company = models.ForeignKey(Company, on_delete=models.CASCADE, default=1)
-    tags = models.ForeignKey(Tags, on_delete=models.CASCADE, default=1)
-    description = models.ForeignKey(Description, on_delete=models.CASCADE, default=1)
-
-
-
-
-
-    
-# class Postsfull(models.Model):
-#     company_info = models.ForeignKey(CompanyInfo, on_delete=models.CASCADE)  # 'app_label.CompanyInfo'
-#     post = models.ForeignKey(Post, on_delete=models.CASCADE)  # 'app_label.Post'
-
-    # salary = models.JSONField(default=list)
-    # salary = models.DecimalField(max_digits=15, decimal_places=2, default=0, default_currency='EUR')
-    
-    # position = models.CharField(max_length=10)
-    # applicants_value = models.SmallIntegerField()
-    # posts_full = models.ForeignKey(Postsfull, on_delete=models.CASCADE)
-
-
-
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    tags = models.ForeignKey(Tags, on_delete=models.CASCADE)
+    description = models.ForeignKey(Description, on_delete=models.CASCADE)
