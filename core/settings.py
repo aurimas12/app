dcbce97fe168ac97538e42c27907de3e150e904b
Jurij Jusel <jjusel@gmail.com>
2023-11-17 20:19:59 +0200

    del migrations changes

--- core/settings.py
+++ core/settings.py
@@ -16,7 +16,7 @@ from decouple import config, AutoConfig
 config = AutoConfig(search_path='.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
