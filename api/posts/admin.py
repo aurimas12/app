6c34e3009ea9910fa658ea92c1b72b1987c91f5f
Jurij Jusel <jjusel@gmail.com>
2023-11-18 23:24:42 +0200

    del download time

--- api/posts/admin.py
+++ api/posts/admin.py
@@ -4,9 +4,8 @@ from .models import Post, Tags, Description
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('register_id', 'post_url', 'position', 'publish_date', 'salary_min', 'salary_max', 'company',)
-    search_fields = ('register_id', 'salary_min', 'salary_max', 'position', 'publish_date', 'created_datetime',) 
-    readonly_fields = ('created_datetime',)
+    list_display = ('register_id', 'post_url', 'position', 'salary_min', 'salary_max', 'company',)
+    search_fields = ('register_id', 'salary_min', 'salary_max', 'position', 'publish_date') 
 
 
 @admin.register(Tags)
--- api/posts/models.py
+++ api/posts/models.py
@@ -11,13 +11,11 @@ class Description(models.Model):
     
 
 class Post(models.Model):
-    register_id = models.IntegerField(null=True)  # post_id keiciamas i register_id
+    register_id = models.IntegerField(null=True)  # post_id pervardinamas i register_id
     post_url = models.CharField(max_length=200)
     position = models.CharField(max_length=200)
-    publish_date = models.DateTimeField(auto_now_add=True)
     salary_min = models.IntegerField(null=True)
     salary_max = models.IntegerField(null=True)
-    created_datetime = models.DateTimeField(auto_now_add=True)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     tags = models.ForeignKey(Tags, on_delete=models.CASCADE)
     description = models.ForeignKey(Description, on_delete=models.CASCADE)
