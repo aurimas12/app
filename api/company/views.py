7a9ca26e7dad8f46e21babd9472d9c7061746d4e
Jurij Jusel <jjusel@gmail.com>
2023-11-20 22:17:21 +0200

    add company, posts views

--- api/company/views.py
+++ api/company/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Company
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import CompanySerializer
 
-# Create your views here.
+
+class CompanyViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Company.objects.all().order_by('-date_joined')
+    serializer_class = CompanySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
--- api/posts/views.py
+++ api/posts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Post
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import PostSerializer
 
-# Create your views here.
+
+class PostViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Post.objects.all().order_by('-date_joined')
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
