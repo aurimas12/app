c330496242a5763dc67b5fb5c1793a21780f6f70
Jurij Jusel <jjusel@gmail.com>
2023-12-03 23:40:37 +0200

    fix: import

--- api/posts/serializers.py
+++ api/posts/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Post, Tags, Description
+from api.posts.models import Post, Tags, Description
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
