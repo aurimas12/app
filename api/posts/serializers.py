0d670e92253a76703b7f85ba723f9d9a4b5eb8a0
Jurij Jusel <jjusel@gmail.com>
2023-12-04 20:49:43 +0200

    feat(serializ): add tags, company to postserializ

--- api/posts/serializers.py
+++ api/posts/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from api.posts.models import Post, Tags, Description
-
+from api.company.serializers import CompanySerializer
 
 class DescriptionSerializer(serializers.ModelSerializer):
     class Meta:
@@ -8,15 +8,19 @@ class DescriptionSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class TagsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tags
+        fields = '__all__'
+
+
 class PostSerializer(serializers.ModelSerializer):
     description = DescriptionSerializer()
+    tags = TagsSerializer()
+    company = CompanySerializer()
     class Meta:
         model = Post
         fields = '__all__'
         
 
-class TagsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Tags
-        fields = '__all__'
 
