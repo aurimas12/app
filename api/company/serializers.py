f93a15131c82b2846bd00c841f8f1833a1e135c5
Jurij Jusel <jjusel@gmail.com>
2023-12-05 19:09:37 +0200

    feat(add): city name serializer

--- api/company/serializers.py
+++ api/company/serializers.py
@@ -3,6 +3,7 @@ from api.company.models import Company
 
 
 class CompanySerializer(serializers.ModelSerializer):
+    city = serializers.CharField(source='get_city_display', read_only=True)
     class Meta:
         model = Company
         fields = '__all__'
