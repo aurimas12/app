c09d509d4b25317d8a145d182093cf18b1696286
Jurij Jusel <jjusel@gmail.com>
2023-12-14 22:36:34 +0200

    feat(urls): del path

--- core/urls.py
+++ core/urls.py
@@ -32,7 +32,6 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'), # 'api/'
-    path('api/', include('api.company.urls')),
     path('api/', include('api.posts.urls')),
 ]
 
