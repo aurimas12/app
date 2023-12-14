3c4c87d877a5cd60f99345b51f50a546a76657d4
Jurij Jusel <jjusel@gmail.com>
2023-12-14 22:28:24 +0200

    fix(ursl): update paths

--- api/posts/urls.py
+++ api/posts/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from api.posts.views import GetAllPosts, GetPostByTag, GetPostById, GetPostByCompany, GetPostByCity
+from api.posts.views import GetAllPosts, GetPostByTag, GetPostByRegisterId, GetPostByCompany, GetPostByCity
 
 
 
 urlpatterns = [
-    path('post/all/', GetAllPosts.as_view(), name='all_posts'),
-    path('post/<int:pk>/', GetPostById.as_view(), name='get_post_by_id'),
+    path('posts/<str:all>/', GetAllPosts.as_view(), name='all_posts'),
+    path('post/<int:register_id>/', GetPostByRegisterId.as_view(), name='get_post_by_id'),
     path('post/tags/<str:language>/', GetPostByTag.as_view(), name='get_posts_by_tag'),  # tag_name
     path('post/company/<str:company_name>/', GetPostByCompany.as_view(), name='get_post_by_company'),
-    path('post/city/<str:city_name>/', GetPostByCity.as_view(), name='get_post_by_city')
+    path('post/city/<str:city>/', GetPostByCity.as_view(), name='get_post_by_city')  # by id exp: vilnius = 1 id = 1
 ]
